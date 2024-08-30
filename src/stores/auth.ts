1f3148bea72d2b807befa187a88c7191d78f995f
JacksonFreire <jakofreire@gmail.com>
2024-08-30 23:30:02 +0200

    fix token define.

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,29 +1,30 @@
-// src/stores/auth.ts
-
 import { defineStore } from 'pinia';
 import authService from '@/services/authService';
 
+interface AuthState {
+  token: string | null;
+}
+
 export const useAuthStore = defineStore('auth', {
-  state: () => ({
-    user: null,
-    token: null,
+  state: (): AuthState => ({
+    token: null, // Inicializamos el estado con token como null
   }),
   actions: {
     async login(username: string, password: string) {
       try {
         const response = await authService.login(username, password);
-        this.token = response.data.token; // Almacena el token de la respuesta
-        // Guardar token en almacenamiento local o de sesión
-        localStorage.setItem('token', this.token);
+        this.token = response.data.token; // Asignamos el token obtenido de la respuesta
+
+        // Asegurarnos de que el valor almacenado en localStorage siempre sea una cadena
+        localStorage.setItem('token', this.token ?? ''); // Utiliza una cadena vacía si el token es null
       } catch (error) {
         console.error('Error al iniciar sesión:', error);
-        throw error;
+        throw error; // Re-lanzamos el error para que pueda ser manejado en otro lugar
       }
     },
     logout() {
-      this.user = null;
-      this.token = null;
-      localStorage.removeItem('token');
+      this.token = null; // Reseteamos el token al cerrar sesión
+      localStorage.removeItem('token'); // Removemos el token del localStorage
     },
   },
 });
